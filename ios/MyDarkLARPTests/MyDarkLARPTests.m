be4ede2c13b289e9dda6bd5b8e7129a1df243ede
Lipokurto <lighting13@yandex.ru>
2023-08-02 13:39:01 +0300

    rename-poject

--- ios/MyDarkLARPTests/MyDarkLARPTests.m
+++ ios/MyDarkLARPTests/MyDarkLARPTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface darkApk03Tests : XCTestCase
+@interface MyDarkLARPTests : XCTestCase
 
 @end
 
-@implementation darkApk03Tests
+@implementation MyDarkLARPTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
